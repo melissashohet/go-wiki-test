f88912cb0e41ea63e75fd941f82b9ba2408632b4
melissashohet <67292695+melissashohet@users.noreply.github.com>
2020-06-23 13:13:24 -0400

    Add files via upload
    First commit

---
+++ wiki.go
@@ -0,0 +1,105 @@
+
+package main
+
+import (
+	"html/template"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"regexp"
+	//"fmt"
+)
+
+type Page struct {
+	Title string
+	Body  []byte
+}
+
+func (p *Page) save() error {
+	filename := "./data/" + p.Title + ".txt"
+	return ioutil.WriteFile(filename, p.Body, 0600)
+}
+
+func loadPage(title string) (*Page, error) {
+	filename := "./data/" + title + ".txt"
+	body, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{Title: title, Body: body}, nil
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
+	p, err := loadPage(title)
+	if err != nil {
+		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
+		return
+	}
+	renderTemplate(w, "view", p)
+}
+
+func editHandler(w http.ResponseWriter, r *http.Request, title string) {
+	p, err := loadPage(title)
+	if err != nil {
+		p = &Page{Title: title}
+	}
+	renderTemplate(w, "edit", p)
+}
+
+func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
+	body := r.FormValue("body")
+	p := &Page{Title: title, Body: []byte(body)}
+	err := p.save()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	http.Redirect(w, r, "/view/"+title, http.StatusFound)
+}
+
+func rootHandler(w http.ResponseWriter, r *http.Request, title string) {
+	renderTemplate(w, "index", nil)
+}
+
+func monkeyHandler(w http.ResponseWriter, r *http.Request, title string) {
+	p := &Page{Title: title, Body: []byte("monkey")}
+	err := p.save()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	http.Redirect(w, r, "/view/"+title, http.StatusFound)
+}
+
+var templates = template.Must(template.ParseFiles("./tmpl/edit.html", "./tmpl/view.html", "./tmpl/index.html"))
+
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+	err := templates.ExecuteTemplate(w, tmpl+".html", p)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+var validPath = regexp.MustCompile("^(/(edit|save|view|monkey)/([a-zA-Z0-9]+))|/$")
+
+func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		m := validPath.FindStringSubmatch(r.URL.Path)
+		//fmt.Println(m)
+		if m == nil {
+			http.NotFound(w, r)
+			return
+		}
+		fn(w, r, m[3])
+	}
+}
+
+func main() {
+	http.HandleFunc("/view/",   makeHandler(viewHandler))
+	http.HandleFunc("/edit/",   makeHandler(editHandler))
+	http.HandleFunc("/save/",   makeHandler(saveHandler))
+	http.HandleFunc("/", 	    makeHandler(rootHandler))
+	http.HandleFunc("/monkey/", makeHandler(monkeyHandler))
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
